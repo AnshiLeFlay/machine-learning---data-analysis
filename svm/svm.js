e84eb5a89414e60560711c290c81cd0f46bbcbd7
AnshiLeFlay <necrofallen@gmail.com>
2016-11-02 16:09:00 +0300

    functions for matrices and some initial data for algorithm

--- svm/svm.js
+++ svm/svm.js
@@ -72,35 +72,166 @@ function mult(mtrxA, mtrxB) {
 	}
 }
 
-function det(mtrx) {
-	var ans;
+/*
+function detG(mtrx) {
+	var ans; 
+
+}
+
+function detT(mtrx) {
+	var ans = 1;
+
+	for (var i = 0; i < mtrx.length; i++) {
+		ans = ans * mtrx[i][i];
+	}
+
+	return ans;
+}
+function minorT(mtrx, n, m) {
+	var ans = 1;
+
+	for (var i = 0; i < mtrx.length; i++) {
+		if (i != n)
+			ans = ans * mtrx[i][i];
+	}
 
 	return ans;
 }
 
 function invert(mtrx) {
 	var ans = [];
+	var det = detT(mtrx);
+	//var minor;
 
-
+	for (var i = 0; i < mtrx.length; i++) {
+		ans[i] = [];
+		for (var j = 0; j < mtrx[0].length) {
+			ans[i][j] = Math.pow(-1, i+j) * minorT(mtrx, i, j);
+		}
+	}
 
 	return ans;
 }
+*/
+//////////////////////////////////////////////////////////////////////////////////////////////
+//will be changed with Gauss method
+//////////////////////////////////////////////////////////////////////////////////////////////
+function Determinant(A)
+{
+    var N = A.length, B = [], denom = 1, exchanges = 0;
+    for (var i = 0; i < N; ++i)
+     { B[i] = [];
+       for (var j = 0; j < N; ++j) B[i][j] = A[i][j];
+     }
+    for (var i = 0; i < N-1; ++i)
+     { var maxN = i, maxValue = Math.abs(B[i][i]);
+       for (var j = i+1; j < N; ++j)
+        { var value = Math.abs(B[j][i]);
+          if (value > maxValue){ maxN = j; maxValue = value; }
+        }
+       if (maxN > i)
+        { var temp = B[i]; B[i] = B[maxN]; B[maxN] = temp;
+          ++exchanges;
+        }
+       else { if (maxValue == 0) return maxValue; }
+       var value1 = B[i][i];
+       for (var j = i+1; j < N; ++j)
+        { var value2 = B[j][i];
+          B[j][i] = 0;
+          for (var k = i+1; k < N; ++k) B[j][k] = (B[j][k]*value1-B[i][k]*value2)/denom;
+        }
+       denom = value1;
+     }
+    if (exchanges%2) return -B[N-1][N-1];
+    else return B[N-1][N-1];
+}
+function AdjugateMatrix(A)
+{                                        
+    var N = A.length, adjA = [];
+    for (var i = 0; i < N; i++)
+     { adjA[i] = [];
+       for (var j = 0; j < N; j++)
+        { var B = [], sign = ((i+j)%2==0) ? 1 : -1;
+          for (var m = 0; m < j; m++)
+           { B[m] = [];
+             for (var n = 0; n < i; n++)   B[m][n] = A[m][n];
+             for (var n = i+1; n < N; n++) B[m][n-1] = A[m][n];
+           }
+          for (var m = j+1; m < N; m++)
+           { B[m-1] = [];
+             for (var n = 0; n < i; n++)   B[m-1][n] = A[m][n];
+             for (var n = i+1; n < N; n++) B[m-1][n-1] = A[m][n];
+           }
+          adjA[i][j] = sign*Determinant(B);
+        }
+     }
+    return adjA;
+}
+function InverseMatrix(A)
+{   
+    var det = Determinant(A);
+    if (det == 0) return false;
+    var N = A.length, A = AdjugateMatrix(A);
+    for (var i = 0; i < N; i++)
+     { for (var j = 0; j < N; j++) A[i][j] /= det; }
+    return A;
+}
+//////////////////////////////////////////////////////////////////////////////////////////////
 
-//X_l, C
-var Xl = [];
-var C = 1;
+var Ls = cholesky(Qss);
+var Ls_inverse = InverseMatrix(Ls);
+var r1 = Ls_inverse * yS;
+var r2 = ;
 
-//2 closest points
-//for example
-var I_s = [];
-var I_o = [];
-var I_c = [];
+function metrics(x1, x2) {
+	var s = 0;
+	for (var i = 0; i < x1.length; i++) {
+		s += Math.pow((x1[i] - x2[i]), 2);
+	}
+	s = Math.sqrt(s);
+	return s;
+}
 
-//solving the quadratic subproblem
-//input data 
-//Q_ss, y_c, y_s, e, c_c
-var Qss = [];
-var Qcc = [];
-var yS = [];
-var yC = [];
-var e = [];
+
+function incasSVM(X, c) {
+	var w, w0;
+	var ans = [];
+
+	var X1 = [], X2 = [];
+	var I_s = [];
+	var i1, i2;
+
+	//
+	for (var n = 0; n < X.length; n++) {
+		if (X[n][1] == 1)
+			X1[X1.length] = X[n];
+		else
+			X2[X2.length] = X[n];
+	}
+
+	//
+	i1 = X1[0][0];
+	var minX1X2 =  metrics(i1, X2[0][0]);
+	for (var m = 1; m < X2.length; m++) {
+		var buf = metrics(i1, X2[m][0]);
+		if (buf < minX1X2) {
+			minX1X2 = buf;
+			i2 = X2[m][0];
+		}
+	}
+	minX1X2 =  metrics(i2, X1[0][0]);
+	for (var m = 1; m < X1.length; m++) {
+		var buf = metrics(i2, X1[m][0]);
+		if (buf < minX1X2) {
+			minX1X2 = buf;
+			i1 = X1[m][0];
+		}
+	}
+
+	//
+	
+
+	ans[0] = w;
+	ans[1] = w0;
+	return ans;
+}
